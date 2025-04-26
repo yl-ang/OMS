db98c54f3e8e14551608054abcdd10bc1e5760ac
yl-ang <yongliangang@gmail.com>
2025-04-26 19:27:45 +0800

    OMS: Prepopulating pool with 100 Order objects

--- src/main/java/org/bobbyangyl/oms/pool/OrderPool.java
+++ src/main/java/org/bobbyangyl/oms/pool/OrderPool.java
@@ -9,12 +9,20 @@ public class OrderPool {
 
     private final Queue<Order> pool = new ConcurrentLinkedQueue<>();
 
+    public OrderPool() {
+        // Pre-populate Obj pool with 100K Order Objects AOT
+        for (int i = 0; i < 100_000; i++) {
+            pool.offer(new Order());
+        }
+    }
+
     public Order acquire() {
         Order order = pool.poll();
-        return (order != null) ? order : new Order();
+        return (order != null) ? order : new Order(); // fallback
     }
 
     public void release(Order order) {
         pool.offer(order);
     }
 }
+
